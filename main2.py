0033fb438c6fcd5515fcd40238833956dd43dce2
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-29 07:25:22 +0700

    Classwork

---
+++ main2.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("main.html")
